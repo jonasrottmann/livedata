3df94504ba39c6cb362f105995f70eaa2cff2e52
Jonas Rottmann <github@jonasrottmann.com>
2019-10-12 20:12:17 +0200

    Add type tests

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,5 +1,5 @@
 import {expectType} from 'tsd'
-import {LiveData, MediatorLiveData, map} from '.'
+import {LiveData, MediatorLiveData, filter, distinct, map} from '.'
 
 expectType<unknown>(new LiveData().get())
 expectType<boolean>(new LiveData(true).get())
@@ -7,7 +7,13 @@ expectType<boolean>(new LiveData().isActive())
 expectType<boolean>(new LiveData(true).set(true))
 expectType<() => void>(new LiveData(true).subscribe(() => {}))
 expectType<void>(new LiveData(true).subscribe(() => true)())
-expectType<LiveData<string>>(map(new LiveData(true), v => 'string'))
+
 expectType<boolean>(new MediatorLiveData(true).get())
 expectType<unknown>(new MediatorLiveData().get())
 expectType<() => void>(new MediatorLiveData().addSource(new LiveData(), v => {}))
+
+expectType<LiveData<string>>(map(new LiveData(true), v => 'string'))
+
+expectType<LiveData<string>>(filter(new LiveData('string'), value => value.length > 0))
+expectType<LiveData<string>>(distinct(new LiveData('string')))
+expectType<LiveData<string>>(distinct(new LiveData('string'), (a, b) => a === b))
